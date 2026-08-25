3ad782db473d7c4f8ecb1b405949827c32e4119f
S-Kirkeby <brozatjepsen@gmail.com>
2026-08-25 20:34:26 +0200

    Added function and random dice exercises

--- ScratchBoard.cpp
+++ ScratchBoard.cpp
@@ -1,68 +1,30 @@
 #include<iostream>
 #include<random>
 
-int Max(int a, int b, int c)
+int aleaIactaEst(std::mt19937 &generator)
 {
-    if(a >= b && a >= c )
-    {
-        return a;
-    }
-    
-    else if(b >= a && b >= c)
-    {
-        return b;
-    }
-    
-    else
-        return c;
-}
-void hilsen()
-{
-    std::cout<<"Hej, og velkommen til Terningespillet!!!" <<std::endl;
-}
+    std::uniform_int_distribution<int> D6 (1, 6);
 
-
-
-int mitTal = 5;
-void SkrivKast(int kast)
-{
-    std::cout<< "Du slog " << kast << " på terningen " <<std:: endl;
-}
-
-int kastTerningen()
-{
-    std::random_device rd;
-    std::mt19937 generator(rd());
-    std::uniform_int_distribution<int> D6(1, 6);
-    
     int kast = D6(generator);
     return kast;
-    
 }
 
-    void forDobbel(int &banan) //Fordi &banan gør banan til en reference til den variabel, der bliver sendt ind til funktionen
-    {
-        banan = banan*2; 
-        
-    }
 
-    void visConst(const int &citron)
-    {
-      
-        std::cout<<"Dette tal er nu som en const, men kun inde i denne funktion "<< citron << std::endl;
-    }
+int point(int a, int b, int c)
+{
+    int resultat = a + b + c;
+    return resultat;
+}
 
 int main()
 {
-    int uforanderlig = 60;
-    hilsen();
-    SkrivKast(5);
-    int resultat = kastTerningen();
-    std::cout<< resultat << std::endl;
-    int tal = 50;
-    std::cout<<"Her er mit oprindelige tal "<< tal << std::endl;
-    forDobbel(tal);
-    std::cout<< "Her er mit nye tal: " << tal << std::endl;
-    visConst(uforanderlig);
-    std::cout<<uforanderlig<<std::endl;
+    std::random_device rd;
+    std::mt19937 generator(rd());
+
+    int kast1 = aleaIactaEst(generator);
+    int kast2 = aleaIactaEst(generator);
+    int kast3 = aleaIactaEst(generator);
+
+    std::cout<< "Kast 1: " << kast1 << " Kast 2: " << kast2 << " Kast 3: " << kast3 << std::endl;
+    std::cout<< point(kast1, kast2, kast3) << std::endl;
 }
