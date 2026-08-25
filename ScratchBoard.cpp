9ae6d15082f9cbee1fb20e1ce9547b026b564700
S-Kirkeby <brozatjepsen@gmail.com>
2026-08-25 07:40:35 +0200

    Added function exercises with & reference

--- ScratchBoard.cpp
+++ ScratchBoard.cpp
@@ -1,45 +1,68 @@
 #include<iostream>
-#include<vector>
-#include<string>
+#include<random>
+
+int Max(int a, int b, int c)
+{
+    if(a >= b && a >= c )
+    {
+        return a;
+    }
+    
+    else if(b >= a && b >= c)
+    {
+        return b;
+    }
+    
+    else
+        return c;
+}
+void hilsen()
+{
+    std::cout<<"Hej, og velkommen til Terningespillet!!!" <<std::endl;
+}
+
+
+
+int mitTal = 5;
+void SkrivKast(int kast)
+{
+    std::cout<< "Du slog " << kast << " på terningen " <<std:: endl;
+}
+
+int kastTerningen()
+{
+    std::random_device rd;
+    std::mt19937 generator(rd());
+    std::uniform_int_distribution<int> D6(1, 6);
+    
+    int kast = D6(generator);
+    return kast;
+    
+}
+
+    void forDobbel(int &banan) //Fordi &banan gør banan til en reference til den variabel, der bliver sendt ind til funktionen
+    {
+        banan = banan*2; 
+        
+    }
+
+    void visConst(const int &citron)
+    {
+      
+        std::cout<<"Dette tal er nu som en const, men kun inde i denne funktion "<< citron << std::endl;
+    }
 
 int main()
 {
-std::string input ="";
-int valg = 0;
-
-    std::vector<std::string>skab = {"tallerken", "kop", "Krus", "kop", "kop", "krus", "Krus"};
-    std::cout<< "4) Delete" <<std::endl;
-    std::cin>>valg;
-
-        for(int i = 0; i < skab.size(); i++)
-        {
-            std::cout<<skab[i]<<std::endl;
-        }
-
-        
-
-        if(valg == 4)
-        
-        std::cout<<" Skriv navnet på den ting du vil slette" <<std::endl;
-        std::cin>>input;
-
-        std::cout<< "Du vil slette "<< input << std::endl;
-        {
-            for(int i = 0; i < skab.size(); i++)
-            if(skab[i] == input)
-            {
-                {
-                    skab.erase(skab.begin()+i);
-                }
-            }
-        }
-
-        std::cout<< "Indholdsliste valgt" << std::endl;
-        std::cout<<" "<<std::endl;
-
-        for(int i = 0; i < skab.size(); i++)
-        {
-            std::cout<<skab[i]<<std::endl;
-        }
-
+    int uforanderlig = 60;
+    hilsen();
+    SkrivKast(5);
+    int resultat = kastTerningen();
+    std::cout<< resultat << std::endl;
+    int tal = 50;
+    std::cout<<"Her er mit oprindelige tal "<< tal << std::endl;
+    forDobbel(tal);
+    std::cout<< "Her er mit nye tal: " << tal << std::endl;
+    visConst(uforanderlig);
+    std::cout<<uforanderlig<<std::endl;
 }
