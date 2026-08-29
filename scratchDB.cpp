ab4aa6806f32d1dd20fd78be1eb22d6a1a605a1a
S-Kirkeby <brozatjepsen@gmail.com>
2026-08-29 21:41:48 +0200

    experiment with SQLite connection and insert

--- scratchDB.cpp
+++ scratchDB.cpp
@@ -1,6 +1,43 @@
 #include<iostream>
+#include<sqlite3.h>
+
+
+int callback(void* data, int argc, char** argv, char** azColName)
+{
+    for (int i = 0; i < argc; i++)
+    {
+        std::cout << azColName[i] << ": "
+                  << (argv[i] ? argv[i] : "NULL")
+                  << std::endl;
+    }
+
+    return 0;
+}
 
 int main()
 {
-    std::cout<<"ScratchBoard Database" << std::endl;
+     
+    sqlite3* db;
+
+    int result = sqlite3_open("koleskab.db", &db);
+
+    if (result != SQLITE_OK)
+    {
+        std::cout << "Could not open database." << std::endl;
+        return 1;
+    }
+
+    std::cout << "Database opened successfully!" << std::endl;
+
+    const char* sql = "INSERT INTO varer (navn) VALUES ('kaffe');";
+    
+
+    result = sqlite3_exec(db, sql, callback, nullptr, nullptr);
+
+    if (result != SQLITE_OK)
+    {
+        std::cout << "SQL error." << std::endl;
+    }
+
+    sqlite3_close(db);
 }
