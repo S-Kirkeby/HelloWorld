d69c5428107e9497f3579035681ecee1fc041ae9
S-Kirkeby <brozatjepsen@gmail.com>
2026-08-29 08:10:39 +0200

    Practice boolean logic and conditions

--- ScratchBoard.cpp
+++ ScratchBoard.cpp
@@ -1,33 +1,57 @@
 #include<iostream>
-#include<vector>
-#include<random>
 
-void aleaIactaEst(std::vector<int> &TerningeKast)
-{
-    std::random_device rd;
-    std::mt19937 generator(rd());
-    std::uniform_int_distribution<int>D6(1, 6);
-
-    int kast = D6(generator);
-
-    TerningeKast.push_back(kast);
-
-}
+ bool erEtPar(int a, int b, int c)
+        {
+        return (a == b && a != c ||
+                a == c && a != b ||
+                b == c && b != a);
+        }
 
 int main()
 {
-    std::vector<int> TerningeKast;
-
-    aleaIactaEst(TerningeKast);
-    aleaIactaEst(TerningeKast);
-    aleaIactaEst(TerningeKast);
-    aleaIactaEst(TerningeKast);
-    aleaIactaEst(TerningeKast);
-
-    for(int tal : TerningeKast)
+     
+    bool testErEtPar = true;
+    bool testErTreEns = true;
+    
+    // Skift kommentaren til de andre sæt af {} for let at skifte test betingelser
     {
-        std::cout<< tal << std::endl;
+    bool testErEtPar = true;
+    bool testErTreEns = false;
+    }
+    //{ Skift kommentaren til de andre sæt af {} for let at skifte test betingelser
+
+    //{ Jeg bruger scope til at holde de konfliktende udtryk ude fra hinanden
+    {
+    bool testErEtPar = false;
+    bool testErTreEns = true;
+    }
+    //} // Skift mellem testscenarierne ved at kommentere de andre sæt ud.
+
+    //{       
+    {
+    bool testErEtPar = false;
+    bool testErTreEns = false;
+    }
+    //}
+
+    if(testErEtPar && !testErTreEns)
+    {
+        std::cout<< "Kun Par testen blev gennemført" << std::endl;
     }
 
-    std::cout << TerningeKast.back() << std::endl; 
+
+    else if(!testErEtPar && testErTreEns)
+    {
+        std::cout<<"Kun 3Ens testen blev gennemført" <<std::endl;
+    }
+    
+    else if(!testErEtPar && !testErTreEns)
+    {
+        std::cout<<"ingen test er gennemført" << std::endl;
+    }
+
+    else if(testErEtPar && testErTreEns)
+    {
+        std::cout<<"Begge test er gennemført" << std::endl;
+    }
 }
